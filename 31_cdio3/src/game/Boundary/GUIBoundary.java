c988909c1fbdcd4c6311159143eb0d1ea0ae8404
Morten <mdue84@gmail.com>
2015-11-17 06:48:07 +0100

    Added GUIBoundary for GUI library indirection

---
+++ 31_cdio3/src/game/Boundary/GUIBoundary.java
@@ -0,0 +1,56 @@
+package game.Boundary;
+
+
+/**
+ * Handles indirection between the supplied GUI and the game
+ */
+public class GUIBoundary implements Outputable{
+
+	
+	/* 
+	 * Updating active player balance and position and displays 
+	 *  the dice rolled by the player
+	 */
+	@Override
+	public void update(int[] dice, int pos, int balance, String playerName) {
+		
+		
+	}
+
+	@Override
+	public void showWelcome() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showWinner(String playerName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String promptPlayerName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void promptRollDice(String playerName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addPlayer(String playerName, int balance) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initializeBoard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
