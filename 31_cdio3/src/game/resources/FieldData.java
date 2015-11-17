54058afded15b1e719bd10a3faaf036aad4029e7
Mathias R. Larsen <mathiaslarsen@10.16.161.195>
2015-11-17 21:46:15 +0100

    rettelser

--- 31_cdio3/src/game/resources/FieldData.java
+++ 31_cdio3/src/game/resources/FieldData.java
@@ -1,5 +1,12 @@
 package game.resources;
 
-public class FieldData {
 
+public class FieldData {
+	
+	public static final int[] FIELDRENT_DATA = {};
+	public static final int[] FIELDBUYPRICE_DATA = {};
+	public static final boolean[] FIELDOWABLE_DATA = {};
+	public static final String[] FIELDTYPE_DATA = {};
+	
+	
 }
