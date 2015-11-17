5615c08fbd3e80e50dda9886cb5f4d4d60b46ec2
Morten <mdue84@gmail.com>
2015-11-17 13:45:17 +0100

    Added App Class for main method

---
+++ 31_cdio3/src/game/App.java
@@ -0,0 +1,11 @@
+package game;
+
+import game.Boundary.GUIBoundary;
+import game.Boundary.Outputable;
+
+public class App {
+	public static void main(String[] args){
+		Outputable output = new GUIBoundary("resources/language.xml");
+		
+	}
+}
