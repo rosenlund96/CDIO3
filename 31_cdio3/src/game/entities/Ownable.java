70e149908cd2e61093f93afd7355e7e2dd7e256a
adamleth <adamleth@gmail.com>
2015-11-17 15:40:20 +0100

    konstruktører

--- 31_cdio3/src/game/entities/Ownable.java
+++ 31_cdio3/src/game/entities/Ownable.java
@@ -5,6 +5,11 @@ public abstract class Ownable {
 	private int price;
 	private Player owner;
 
+	public Ownable(Field field, int price, Player owner) {
+		this.price = price;
+		this.owner = owner;
+	}
+	
 	
 	public int getRent() {
 		return getRent();
