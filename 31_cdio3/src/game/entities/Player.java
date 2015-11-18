be8a262fbeb28d63c30654f8dd54467f895c2c64
Brian Christensen <Allegodenavnevartaget@hotmail.com>
2015-11-18 14:11:43 +0100

    Player & Balance klassen er lavet

--- 31_cdio3/src/game/entities/Player.java
+++ 31_cdio3/src/game/entities/Player.java
@@ -2,54 +2,41 @@ package game.entities;
 
 public class Player {
 
-	private String name; 
-	private int position;
+
+	// Attributes
+
+	private int position = 0;
+	private String name;
 	private Balance balance;
-	private boolean extraTurn;
-	
-	
+
+	// Constructors
+
 	public Player(String name, int startingBalance){
 		this.name = name;
 		this.balance = new Balance(startingBalance);
-		this.position = 0; 
 	}
-	
-	//returnere navnet på den nuværende spiller. 
-	public String getName(){ 
+
+	// Mutators
+
+	public String getName(){
 		return name;
 	}
-	
-	//ændre balancen på den nuværende spiller. 
-	public void changeBalance(int deltaBalance){ 	 	
-			balance.changeBalance(deltaBalance);
-	}
-	
-	public void setBalance(int balance){
-		this.balance.setBalance(balance);
-	}
-	
-	//returnere den nuværende balance på spilleren. 
-	public int getBalance(){
-		return balance.getBalance();
-	}
-	
-	public void setPosition(int newPosition) {
-		// player can only land on field 2 to 12
-		if(newPosition > 1 && newPosition < 13)
-			this.position = newPosition; 
-	}
-	//returnere den nuværende position på spilleren
-	public int getPosition(){
-		return position; 
-	}
-	
-	public void setExtraTurn(boolean extraTurn){
-		this.extraTurn = extraTurn;
-	}
-	
-	public boolean getExtraTurn(){
-		return this.extraTurn;
-	}
-	
-}
 
+	public int getPosition(){
+		return position;
+	}
+
+	public void setPosition(int position){
+		this.position = position;		
+	}
+
+	public void deposit(int amount){
+		balance.deposit(amount);
+
+	}
+
+	public int withdraw(int amount){
+		return balance.withdraw(amount);
+	}
+
+}
