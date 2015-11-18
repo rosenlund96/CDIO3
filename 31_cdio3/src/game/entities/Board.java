dc1e2cc26626aabe38a235dbc7391e13b4ce6c4c
II <II@10.16.161.11>
2015-11-18 02:53:16 +0100

    Added contructors and methods for Board.java

--- 31_cdio3/src/game/entities/Board.java
+++ 31_cdio3/src/game/entities/Board.java
@@ -1,9 +1,33 @@
 package game.entities;
 
 public class Board {
-	
-	
-	//Git test
-	
+
+	Board(String[] names, int startingBalance, Gui Boundary){
+
+	}
+
+
+
+	public void moveActivePlayer(int amount ){
+
+	}
+
+
+	public void findStartingPlayer(){
+
+	}
+
+	public boolean getWinner(){
+
+	}
+
+
+	public int getActiveplayerPosition(){
+
+	}
+
+	public int getActivePlayerBalance(){
+
+	}
 
 }
