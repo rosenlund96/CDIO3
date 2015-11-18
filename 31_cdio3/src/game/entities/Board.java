f8efdcf6e37aed15aadca3b9c65271e22b884477
Casper Danielsen <kiwicasper@hotmail.com>
2015-11-18 15:11:22 +0100

    Changes

--- 31_cdio3/src/game/entities/Board.java
+++ 31_cdio3/src/game/entities/Board.java
@@ -8,16 +8,19 @@ public class Board {
 	
 	
 	ArrayList<Player> players;
+	Player activePlayer;
+	int numberOfPlayers;
+	
 	// Konstruktor
 	public Board(String[] names, int startingBalance, Outputable gui){
 		for (int i = 0; i < names.length; i++) {
 			players.add(new Player(names[i], startingBalance));
 		}
 		FieldManager fm = new FieldManager(gui);
-		
-		
+		numberOfPlayers = names.length;
 	}
-
+	
+	// Finds who gets to start
 
 
 	public void moveActivePlayer(int amount ){
