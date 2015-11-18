f6cf147725265407383983aef7868b63b9c5f0ad
II <II@10.16.161.11>
2015-11-18 15:48:49 +0100

    Find starting player

--- 31_cdio3/src/game/entities/Board.java
+++ 31_cdio3/src/game/entities/Board.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 import game.Boundary.Outputable;
 
 public class Board {
-	
-	
+
+
 	ArrayList<Player> players;
 	Player activePlayer;
 	int numberOfPlayers;
-	
+
 	// Konstruktor
 	public Board(String[] names, int startingBalance, Outputable gui){
 		for (int i = 0; i < names.length; i++) {
@@ -19,18 +19,21 @@ public class Board {
 		FieldManager fm = new FieldManager(gui);
 		numberOfPlayers = names.length;
 	}
-	
-	// Finds who gets to start
+
 
 
 	public void moveActivePlayer(int amount ){
-		
+		activePlayer.setPosition((activePlayer.getPosition()+ amount)%fm.getNumberOfFields() );
 
 	}
 
+	// Finds who gets to start
 
 	public void findStartingPlayer(){
 
+		int random = ((int)Math.random() * numberOfPlayers);
+		activePlayer = players.get(random);
+		
 	}
 
 	public boolean getWinner(){
