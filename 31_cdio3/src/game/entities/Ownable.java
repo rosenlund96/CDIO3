85ebace7076df4a3e03e04ce0d9c2a99e3d1ab82
Henrik <Henrik@10.16.174.39>
2015-11-20 09:42:46 +0100

    indrykning

--- 31_cdio3/src/game/entities/Ownable.java
+++ 31_cdio3/src/game/entities/Ownable.java
@@ -13,37 +13,37 @@ public abstract class Ownable extends Field {
 		this.price = price;
 		this.rent = rent; 
 	}
-	
+
 	public int getPrice(){
 		return price;
 	}
-	
+
 	@Override
 	public void landOnField(Player player){
 		//Hvis feltet er ledigt
 		if(this.owner == null){
-		int balance = player.getBalance();
-		if(balance >= price) {
-		output.promptBuy(player.getName(), price);
-		player.withdraw(price);	
-		}
-		else {
-			output.showNotEnoughBalanceMessage(player);
-		}
+			int balance = player.getBalance();
+			if(balance >= price) {
+				output.promptBuy(player.getName(), price);
+				player.withdraw(price);	
+			}
+			else {
+				output.showNotEnoughBalanceMessage(player);
+			}
 		}
 	}
-	
-	
+
+
 	public void setOwner(Player owner){
 		this.owner = owner; 
 	}
-	
+
 	public Player getOwner(){
 		return owner; 
 	}
-	
-	
+
+
 	// fuck det her pis
 	public abstract int getRent();
-	
+
 }
