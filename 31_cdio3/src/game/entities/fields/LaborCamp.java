8f204db73422e9f7f0bbfa3db38cf8ef42931762
Morten <mdue84@gmail.com>
2015-11-21 20:35:19 +0100

    LaborCamp class errors corrected

---
+++ 31_cdio3/src/game/entities/fields/LaborCamp.java
@@ -0,0 +1,51 @@
+package game.entities.fields;
+
+import game.boundaries.Outputable;
+import game.entities.FieldManager;
+import game.entities.Player;
+import game.util.DieCup;
+
+public class LaborCamp extends Ownable {
+
+
+	private DieCup dices;		// for rent calculation 
+
+	public LaborCamp(FieldManager fieldManager, int price, int baseRent, Outputable output ) {
+		super(fieldManager, FieldType.LABOR_CAMP, price, baseRent, output);
+		dices = new DieCup();
+	}
+	
+	 
+	public void landOnField(Player player){
+		
+		// checks if the field is owned
+		if (this.owner == null) {
+			super.landOnField(player);
+		}
+		
+		// checks if the player is the owner if not. pay rent
+		else if (this.owner != player) {
+			// check how many LABOR CAMPS owner has
+			int fieldsOwned = fieldManager.getFieldsOwned(owner, FieldType.LABOR_CAMP);
+			output.showRollingDiceForRent(player.getName());
+			
+			int amountToPay = dices.roll() * rent * fieldsOwned; 
+			transferRent(amountToPay, player);
+		}
+		// checks if the player is the owner
+		else if (this.owner == player) {
+			output.showPlayerIsOwner(player.getName());
+
+		}
+	}
+	
+	
+	// method to transfer money from the player to the owner of the field
+	public void transferRent(int amountToPay, Player player){
+		// withdraws the due rent or the rest of the players balance if he/she can't afford it 
+		int withdrawAmount = player.withdraw(amountToPay);
+		// deposits the withdrawed money
+		owner.deposit(withdrawAmount);
+		output.showTransferMessage(player.getName(), owner.getName(), withdrawAmount);
+	}
+}
