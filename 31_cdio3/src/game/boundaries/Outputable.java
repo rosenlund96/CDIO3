5e4b90d152f8f55ce94ffa0b763e08b80aaebd83
Casper Danielsen <kiwicasper@hotmail.com>
2015-11-23 15:12:44 +0100

    Made it pretty

--- 31_cdio3/src/game/boundaries/Outputable.java
+++ 31_cdio3/src/game/boundaries/Outputable.java
@@ -1,12 +1,11 @@
 package game.boundaries;
 
-import game.entities.Player;
 
-/**
- * 
- * Interface defining methods for input output to screen from game
- * 
- */
+/************************************************************************
+ * 																		*
+ * Interface defining methods for input output to screen from game		*
+ * 																		*
+ ************************************************************************/
 public interface Outputable {
 	
 	public void update(int[] dice, int pos, int balance, String playerName);
@@ -20,22 +19,37 @@ public interface Outputable {
 	public void removeOwner(int fieldNumber);
 
 	public void showUpdateMessage(String playerName, int pos);
+	
 	public void showWelcome();
+	
 	public void showStartingPlayer(String playerName);
+	
 	public void showWinner(String playerName);
+	
 	public void showWithdrawMessage(String playerName, int amount);
+	
 	public void showDepositMessage(String playerName, int bonus);
+	
 	public void showTransferMessage(String playerName, String ownerName, int amount);
+	
 	public void showNotEnoughBalanceMessage(String playerName);
+	
 	public void showBrokeMessage(String playerName);
+	
 	public void showNotBoughtMessage(String playerName);
+	
 	public void showFieldBoughtMessage(String playerName, int fieldNumber);
+	
 	public void showRollingDiceForRent(String playeName);
+	
 	public void showPlayerIsOwner(String playerName);
 	
 	public String promptPlayerName(int playerNumber, boolean error);
+	
 	public void promptRollDice(String playerName);
+	
 	public boolean promptTax(String playerName, int taxAmount, int percentAmount);
+	
 	public boolean promptBuy(String name, int price);
 	
 	public void initializeBoard();
