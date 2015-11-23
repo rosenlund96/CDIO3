3a7992668fadbffcbf8b6933b86b7e7a035e8f47
Brian Christensen <Allegodenavnevartaget@hotmail.com>
2015-11-23 14:23:52 +0100

    Removed Static modifiers

--- 31_cdio3/src/game/entities/fields/Fleet.java
+++ 31_cdio3/src/game/entities/fields/Fleet.java
@@ -6,10 +6,10 @@ import game.entities.Player;
 
 public class Fleet extends Ownable {
 
-	private static final int RENT_1 = 500;
-	private static final int RENT_2 = 1000;
-	private static final int RENT_3 = 2000;
-	private static final int RENT_4 = 4000;
+	private final int RENT_1 = 500;
+	private final int RENT_2 = 1000;
+	private final int RENT_3 = 2000;
+	private final int RENT_4 = 4000;
 
 	public Fleet(FieldManager fieldManager, int price, Outputable output) {
 		super(fieldManager, FieldType.FLEET, price, 500, output);
